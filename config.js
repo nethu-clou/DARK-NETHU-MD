2b9e262bd56e28aa7fc3f91a74a8e149c63c3e89
nethu-clou <isurudineth49@gmail.com>
2024-10-14 16:54:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID|| "uKoinIJR#dF4KuBLZz5p-BedwOYb_Z8t3Pe50uUwYU6YMrg-RxC0",
+SESSION_ID: process.env.SESSION_ID|| "F783RBDY#dWGUnPbgVx6tCR3Do8vzdwOk58E3fGGtsFQUMfrBLTI",
 MONGODB: process.env.MONGODB|| "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
